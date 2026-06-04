75874d9e1c112e78ed7fad7f365921ab9e3d0872
naoyoshi001 <naoyochism@icloud.com>
2026-06-04 16:39:15 +0900

    ファイル作成

---
+++ frontend/app/api/device_types/fetchDeviceTypes.ts
@@ -0,0 +1,22 @@
+import { API_BASE_URL } from "../client"
+
+export async function getDeviceTypesFromApi()
+{
+    console.log("fetchDeviceTypes")
+
+    const token = localStorage.getItem("access_token")
+    if (!token) {return}
+
+    const response = await fetch(
+                        `${API_BASE_URL}/device-types`,
+                        {
+                          method: "GET",
+                          headers: {
+                                      Authorization:
+                                        `Bearer ${token}`
+                                    }
+                        }
+                      )
+
+    return await response.json()
+}
