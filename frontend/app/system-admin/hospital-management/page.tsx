0827f87bd65bece7e584b042e8f55b75e462ca24
naoyoshi001 <naoyochism@icloud.com>
2026-07-09 14:51:44 +0900

    空ファイル削除

--- frontend/app/system-admin/hospital-management/page.tsx
+++ frontend/app/system-admin/hospital-management/page.tsx
@@ -33,7 +33,7 @@ useEffect(
             () => {
                     fetchHospitalManagementTransaction({
                                                           setHospitals,
-                                                          setFilteredHospitals
+                                                          //setFilteredHospitals
                                                         })
                   },
             []
