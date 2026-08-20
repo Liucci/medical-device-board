63a68003dff4e844c56280090bf10366d0c6034d
naoyoshi001 <naoyochism@icloud.com>
2026-08-20 23:07:30 +0900

    refresh token 修正

--- backend/auth/refresh_token.py
+++ backend/auth/refresh_token.py
@@ -39,22 +39,3 @@ def refresh_token(session_id: str,):
     except Exception:
         raise
 
-"""
-def refresh_token(refresh_token: str):
-    print("refresh_token")
-    #print("received refresh token", refresh_token[:12])
-
-    try:
-        #print("before refresh", get_auth_client_for_login.postgrest.headers)
-        response = get_auth_client_for_login().auth.refresh_session(refresh_token)
-        
-        #print("after refresh", get_auth_client_for_login.postgrest.headers)
-        #print("refresh_session success")
-        return response
-
-    except Exception as e:
-        #print("refresh_session exception")
-        #print(type(e))
-        #print(repr(e))
-        raise
-"""
